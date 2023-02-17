400f4970b1075711d2a24e731ea47ad52bcba8af
goga123456 <110159417+goga123456@users.noreply.github.com>
2023-02-17 14:45:51 +0500

    Update bot.py

--- app/bot.py
+++ app/bot.py
@@ -1658,7 +1658,7 @@ def send_email():
     msg.attach(MIMEText(html, 'html', 'utf-8'))
         
     #filename='example.xlsx'
-    fp=open('example.xlsx','rb')
+    fp=open('C:\Users\User\Desktop\movarounahr_bot-master\app\example.xlsx','rb')
     att = email.mime.application.MIMEApplication(fp.read(),_subtype="xlsx")
     fp.close()
     att.add_header('Content-Disposition','attachment',filename=filename)
